73634d95ddd4f7ffc6e51588ee8b5ada5110e0e5
jutionck <30655440+jutionck@users.noreply.github.com>
2020-07-10 21:51:23 +0700

    [CHALLENGE] add

--- DAY#2/challenge/tagihanRestorant.js
+++ DAY#2/challenge/tagihanRestorant.js
@@ -4,30 +4,26 @@ let tips = [10, 15, 20];
 
 let Pembayaran = (restaurant) => {
     // declare all variable
-    let i = 0, lenRestaurant = restaurant.length, tipsAmount, subTotal, arr = [];
+    let i = 0, lenRestaurant = restaurant.length, tipsAmount, total;
     // loop restaurant variable
     for(; i < lenRestaurant;) {
         // check price to give tips
         if(restaurant[i] > 500000) {
             tipsAmount = restaurant[i] * (tips[0] / 100);
-            subTotal = restaurant[i] + tipsAmount
+            total = restaurant[i] + tipsAmount
         } else if(restaurant[i] >=250000 && restaurant[i] <= 500000) {
             tipsAmount = restaurant[i] * (tips[1] / 100);
-            subTotal = restaurant[i] + tipsAmount
+            total = restaurant[i] + tipsAmount
         } else {
             tipsAmount = restaurant[i] * (tips[2] / 100);
-            subTotal = restaurant[i] + tipsAmount
+            total = restaurant[i] + tipsAmount
         }
-        // make arr is object use method push
-        arr.push({
-            Tagihan: restaurant[i],
-            Tips: tipsAmount,
-            Total: subTotal
-        })
+        console.log("Tagihan  :" + restaurant[i]);
+        console.log("Tips \t :" + tipsAmount);
+        console.log("Total \t :" + total);
+        console.log("-----------------------------")
         i++
     }
-    // print result
-    console.log(arr)
 }
 
 // call function to display
